929f8dbacc572f4ede201c963cdc3631ba38da3e
Krishanu Dev <kdev@genexis.eu>
2024-11-01 05:13:19 +0530

    section7.7

--- test_configdata.py
+++ test_configdata.py
@@ -1,4 +1,10 @@
 from pytest_topics.utils.myconfigparser import *
+from pytest_topics.utils.configFileParser import ConfigFileParser
+
+config = ConfigFileParser('prod.ini')
 
 def test_getgmailurl():
 	print(getGmailUrl())
+
+def test_getoutlookurl():
+	print(config.getOutlookUrl())
